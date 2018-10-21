39096c6fc7d987296f56f622d2b13a30ff5dca1c
Steven Inouye <steveninouye@msn.com>
2018-10-21 08:44:23 -0700

    added types for index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const typeDefs = `
 
 const resolvers = {
     Query: {
-        hello: (_, { name }) => `Hello ${name || 'World'}`
+        hello: (_: any, { name }: any) => `Hello ${name || 'World'}`
     }
 };
 
